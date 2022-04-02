61521f72e6b3797953ab07f964fd49fad5029f9a
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2022-04-02 11:22:15 +0300

    Shellcheck

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ fi
 # Download syncthing
 if [ ! -f ~/Programs/syncthing/syncthing ]; then
     URL="$(curl -s https://api.github.com/repos/syncthing/syncthing/releases/latest \
-        | grep "https://.*syncthing-linux-amd64.*tar\.gz" \ 
+        | grep "https://.*syncthing-linux-amd64.*tar\.gz" \
         | cut -d : -f 2,3 \
         | tr -d " ")"
     file-get "$URL" ~/Programs/syncthing.tar.gz
